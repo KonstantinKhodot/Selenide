d2b79470a931f75842fb53b948c148a28a53af58
KonstantinKhodot <Khodot.konstantin@mail.ru>
2022-11-28 19:55:23 +0300

    correction1

--- src/test/java/runetologyselenide/SelenideTest.java
+++ src/test/java/runetologyselenide/SelenideTest.java
@@ -1,23 +1,38 @@
 package runetologyselenide;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 
 class SelenideTest {
+    String planningDate = generateDate(15);
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @Test
     void validTest() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[placeholder=\"Город\"]").setValue("Москва");
-        $x("input[placeholder=\"Дата встречи\"]");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[name=\"name\"]").setValue("Иван Петров-Иванов");
         $("[name=\"phone\"]").setValue("+79119526138");
         $("[data-test-id='agreement']").click();
+        $("[data-test-id='date'] input").setValue(planningDate);
         $x("//span[@class=\"button__text\"]").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
         String texts = $("[data-test-id=\"notification\"]").getText();
     }
+
 }
